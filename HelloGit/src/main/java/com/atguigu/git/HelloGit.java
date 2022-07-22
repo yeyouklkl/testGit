85a9c12ebf6337ad3257b49121eeff90b62842c2
yeyouklkl <105710700+yeyouklkl@users.noreply.github.com>
2022-07-22 13:23:08 +0800

    Update HelloGit.java

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -8,6 +8,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("创建分支");
+        System.out.println("在线编辑");
     }
 
 }
